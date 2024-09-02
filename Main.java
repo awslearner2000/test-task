e0888116e9dd47418f970c1753510d441410e54d
Abilda Alimbay <abilda.alimbay@booking.com>
2024-09-02 15:36:58 +0200

    calculated min time per carrier

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import models.Ticket;
 import utils.FileReader;
@@ -10,7 +12,10 @@ import utils.TimeDiffCalculator;
 public class Main {
     public static void main(String[] args) {
         List<Ticket> ticketList = FileReader.readFile("/Users/aalimbay/Desktop/abilda/projects/test1/src/tickets.json");
-        System.out.println("Min flight time is - " + findMinTime(ticketList));
+        Map<String, Integer> flightTimesPerCarrier = findMinTimePerCarrier(ticketList);
+        for (var iter : flightTimesPerCarrier.entrySet()) {
+            System.out.println(iter.getKey() + " " + iter.getValue());
+        }
         List<Double> prices = new ArrayList<>();
         Double totalPrice = 0.0;
         for (Ticket ticket : ticketList) {
@@ -30,19 +35,25 @@ public class Main {
             double middle2 = prices.get(size / 2);
             medianPrice = (middle1 + middle2)/2;
         }
-        System.out.println("Median price is - " + medianPrice);
         System.out.println("The difference between median and average price is - " + Math.abs(averagePrice-medianPrice));
     }
 
-    public static int findMinTime(List<Ticket> ticketList) {
-        int minTime = Integer.MAX_VALUE;
+    public static Map<String, Integer> findMinTimePerCarrier(List<Ticket> ticketList) {
+        Map<String, Integer> carrierToMinFlightTime = new HashMap<>();
         for (Ticket ticket : ticketList) {
             if (ticket.getOrigin_name().equals("Владивосток") && ticket.getDestination_name().equals("Тель-Авив")) {
+                String mapKey = ticket.getCarrier();
                 int flightTime = TimeDiffCalculator.getTimeDiffInMinutes(ticket.getDeparture_date(),
                     ticket.getDeparture_time(), ticket.getArrival_date(), ticket.getArrival_time());
-                minTime = Math.min(minTime, flightTime);
+                if (carrierToMinFlightTime.containsKey(mapKey)) {
+                    int minTime = Math.min(flightTime, carrierToMinFlightTime.get(mapKey));
+                    carrierToMinFlightTime.put(mapKey, minTime);
+                } else {
+                    carrierToMinFlightTime.put(mapKey, flightTime);
+                }
             }
         }
-        return minTime;
+        return carrierToMinFlightTime;
     }
+
 }
